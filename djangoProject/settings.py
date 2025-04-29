a6511de7a9e3450e575b45ff06bbe49e1e78b9e9
Xavi <ravirubio2621@gmail.com>
2025-04-29 19:03:41 +0200

    Change settings for database

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -95,11 +95,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
